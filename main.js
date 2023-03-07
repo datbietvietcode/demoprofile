20bbe4e11e89113ce705368f0b4b966da7e7a677
datbietvietcode <ngthedat973@gmail.com>
2023-03-08 01:01:38 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing", {
+    strings:["","Web Disigner .","Frontend Developer .","Web Developer .","Vietnamese Programmer ."],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
